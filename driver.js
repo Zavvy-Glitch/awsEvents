223c7e7a712a570c99392b4e6ddccbc1aa3b55f7
chattray <chea0073@gmail.com>
2021-11-11 18:27:46 -0800

    Vendor / Recieve completed

---
+++ driver.js
@@ -0,0 +1,24 @@
+// Import required AWS SDK clients and commands for Node.js
+const { ReceiveMessageCommand } = require("@aws-sdk/client-sqs");
+const { sqsClient } = require("./libs/sqsClient.js");
+
+const queueUrl = "package";
+
+const params = {
+  AttributeNames: ["SentTimestamp"],
+  MaxNumberOfMessages: 1,
+  MessageAttributeNames: ["All"],
+  QueueUrl: queueUrl,
+  WaitTimeSeconds: 10,
+  };
+
+const run = async () => {
+  try {
+    const data = await sqsClient.send(new ReceiveMessageCommand(params));
+    if (data.Messages) 
+   console.log(data); // For unit tests.
+  } catch (err) {
+    console.log("Receive Error", err);
+  }
+};
+run();
